2fdf5171b5d08e485ecd3bf51c1ebca26850c2f2
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-13 13:39:49 +0000

    Add bcrypt for password management

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const users = require("./usersModel");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
 router.post("/register", (req, res) => {
-  const user = req.body;
+  const user = {
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, 10)
+  };
   users
     .addUser(user)
     .then(data => res.status(201).json(data))
@@ -12,10 +16,16 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
   users
     .getUser(username)
-    .then(data => res.status(201).json(data))
+    .then(data => {
+        if (bcrypt.compareSync(password, data.password)) {
+            res.status(200).json("You're logged in!");
+        } else {
+            res.status(400).json("Invalid credentials.");
+        }
+    })
     .catch(error => res.status(500).json({ message: error.message }));
 });
 
