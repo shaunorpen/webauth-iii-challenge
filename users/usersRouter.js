f7d7f28ce94e5c8881c55e83c4364e8c1c1e2b55
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-13 13:59:52 +0000

    Add jwt

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const users = require("./usersModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 router.post("/register", (req, res) => {
   const user = {
-      username: req.body.username,
-      password: bcrypt.hashSync(req.body.password, 10)
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 10)
   };
   users
     .addUser(user)
@@ -16,24 +17,50 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, department } = req.body;
+  const token = generateToken({ username, department });
   users
     .getUser(username)
     .then(data => {
-        if (bcrypt.compareSync(password, data.password)) {
-            res.status(200).json("You're logged in!");
-        } else {
-            res.status(400).json("Invalid credentials.");
-        }
+      if (bcrypt.compareSync(password, data.password)) {
+        res
+          .status(200)
+          .json({ message: "Welcome, you're logged in!", token: token });
+      } else {
+        res.status(400).json("Invalid credentials.");
+      }
     })
     .catch(error => res.status(500).json({ message: error.message }));
 });
 
 router.get("/users", (req, res) => {
-  users
-    .getUsers()
-    .then(data => res.status(200).json(data))
-    .catch(error => res.status(500).json({ message: error.message }));
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(
+      token,
+      "My secret string which I keep safe, not here!",
+      error => {
+        if (error) {
+          res.status(401).json({ message: error.message });
+        } else {
+          users
+            .getUsers()
+            .then(data => res.status(200).json(data))
+            .catch(error => res.status(500).json({ message: error.message }));
+        }
+      }
+    );
+  } else {
+    res.status(400).json({ message: "No token supplied" });
+  }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.username,
+    department: user.department
+  };
+  return jwt.sign(payload, "My secret string which I keep safe, not here!");
+}
 
 module.exports = router;
